839ded988500ba599870b708ddc66d1d37154795
deja21k <thedoctor92@hotmail.com>
2017-04-16 18:41:25 +0200

    Generated Model and Controller for Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416161702) do
+ActiveRecord::Schema.define(version: 20170416163456) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
